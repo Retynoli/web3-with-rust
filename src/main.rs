4685a8f29f05401751e11435c4683d9062bb19bd
retynoli <retynoli@gmail.com>
2022-06-05 21:23:41 +0300

    OKOKU contract added

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ use web3::{
 
 #[tokio::main]
 async fn main() -> web3::Result<()> {
-
     let transport = Http::new("http://localhost:8545")?;
     let web3 = web3::Web3::new(transport);
     let accounts = web3.eth().accounts().await?;
@@ -18,27 +17,42 @@ async fn main() -> web3::Result<()> {
     let bytecode = include_str!("./res/KOKU.bin");
     let abi = include_bytes!("./res/KOKU.abi");
 
-    // Deploying a contract
-    let contract = Contract::deploy(web3.eth(), abi).unwrap()
+    // Deploying a KOKU contract
+    let koku_contract = Contract::deploy(web3.eth(), abi).unwrap()
         .confirmations(0)
         .poll_interval(time::Duration::from_secs(1))
         .options(Options::with(|opt| opt.gas = Some(3_000_000.into())))
         .execute(bytecode, accounts[0], accounts[0])
         .await.unwrap();
 
-    println!("Deployed at: {}", contract.address());
+    println!("Deployed at: {}", koku_contract.address());
+
+    // Get the contract bytecode and ABI from Solidity compiler
+    let bytecode = include_str!("./res/OKOKU.bin");
+    let abi = include_bytes!("./res/OKOKU.abi");
+
+    // Deploying an OKOKU contract
+    let okoku_contract = Contract::deploy(web3.eth(), abi).unwrap()
+        .confirmations(0)
+        .poll_interval(time::Duration::from_secs(1))
+        .options(Options::with(|opt| opt.gas = Some(3_000_000.into())))
+        .execute(bytecode, (), accounts[0])
+        .await.unwrap();
+
+    println!("Deployed at: {}", okoku_contract.address());
 
     // // If we already have a deployed contract
     // let token_address = Address::from_str("0xdEa937d22a19AFd39Aa88DdC72fF8859a102B0C3").unwrap();
     // let contract = Contract::from_json(web3.eth(), token_address, abi).unwrap();
 
-    let owner: Address = contract.query("owner", (), None, Options::default(), None).await.unwrap();
-    println!("Owner: {:?}", owner);
+    // // Check owner address after deployment
+    // let owner: Address = koku_contract.query("owner", (), None, Options::default(), None).await.unwrap();
+    // println!("Owner: {:?}", owner);
 
     // Transfer some tokens from one account to another
-    println!("Balance before transfer: {:?}", balance(&contract, accounts[1]).await);
-    let tx = transfer(&contract, accounts[1], accounts[0]).await;
-    println!("Balance after transfer: {:?}", balance(&contract, accounts[1]).await);
+    println!("Balance before transfer: {:?}", balance(&koku_contract, accounts[1]).await);
+    let tx = transfer(&koku_contract, accounts[1], accounts[0]).await;
+    println!("Balance after transfer: {:?}", balance(&koku_contract, accounts[1]).await);
     println!("Transaction hash: {:?}", tx);
 
 
